e9f741c6ce94633a2694a9dafd2e565c0ee8e1d6
issawayzani <d799888@gmail.com>
2023-03-18 00:12:41 +0200

    ui changes

--- Frontend-v1-Original/components/ssSwap/ssSwap.tsx
+++ Frontend-v1-Original/components/ssSwap/ssSwap.tsx
@@ -1,13 +1,13 @@
-import { Paper } from "@mui/material";
+import { Box} from "@mui/material";
 import Setup from "./setup";
 import classes from "./ssSwap.module.css";
 
 function Swap() {
   return (
     <div className={classes.newSwapContainer}>
-      <Paper elevation={0} className={classes.swapContainer}>
+      <Box className={classes.swapContainer}>
         <Setup />
-      </Paper>
+      </Box>
     </div>
   );
 }
