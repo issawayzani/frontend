8aa4fcdf56f5d243642094df1e19ceb12dbdaf3d
issawayzani <d799888@gmail.com>
2023-03-28 02:28:31 +0300

    change design of liquidity before connect metamask

--- Frontend-v1-Original/pages/liquidity/create/index.tsx
+++ Frontend-v1-Original/pages/liquidity/create/index.tsx
@@ -1,9 +1,80 @@
 import LiquidityCreate from "../../../components/ssLiquidityManage/ssLiquidityManage";
-
+import Unlock from "../../../components/unlock/unlockModal";
+import { ACTIONS } from "../../../stores/constants/constants";
+import { Button, Paper, Typography } from "@mui/material";
+import stores from "../../../stores";
+import { useEffect, useState } from "react";
+import { BungalowTwoTone } from "@mui/icons-material";
 function Pair() {
+  const accountStore = stores.accountStore.getStore("account");
+  const [account, setAccount] = useState(accountStore);
+  const [unlockOpen, setUnlockOpen] = useState(false);
+ 
+  useEffect(() => {
+    const accountConfigure = () => {
+      const accountStore = stores.accountStore.getStore("account");
+      setAccount(accountStore);
+      closeUnlock();
+    };
+    const connectWallet = () => {
+      onAddressClicked();
+    };
+
+    stores.emitter.on(ACTIONS.ACCOUNT_CONFIGURED, accountConfigure);
+    stores.emitter.on(ACTIONS.CONNECT_WALLET, connectWallet);
+    return () => {
+      stores.emitter.removeListener(
+        ACTIONS.ACCOUNT_CONFIGURED,
+        accountConfigure
+      );
+      stores.emitter.removeListener(ACTIONS.CONNECT_WALLET, connectWallet);
+    };
+  }, []);
+
+  const onAddressClicked = () => {
+    setUnlockOpen(true);
+  };
+
+  const closeUnlock = () => {
+    setUnlockOpen(false);
+  };
   return (
+    <div>
+      {account && account.address ? (
     <div className="h-100 w-100 mb-16 mt-0 flex flex-col pt-28 sm:pt-36 lg:pt-14">
       <LiquidityCreate />
+    </div>
+      ) : (
+        <Paper className="fixed top-0 flex h-[calc(100%-150px)] w-[calc(100%-80px)] flex-col flex-wrap items-center justify-center bg-[rgba(17,23,41,0.2)] p-12 text-center shadow-none max-lg:my-auto max-lg:mt-24 max-lg:mb-0 lg:h-[100vh] lg:w-full">
+          <div className="relative z-10">
+            <Typography
+              className="text-center font-montserrat text-2xl font-thin text-white sm:text-3xl"
+              variant="h1"
+            >
+              Add liquidity
+            </Typography>
+            <Typography
+              className="color-[#7e99b0] my-7 mx-auto max-w-3xl text-center text-base sm:text-lg"
+              variant="body2"
+            >
+              Add liquidity to existing stable or volatile
+              Liquidity Pairs.
+            </Typography>
+            <Button
+              disableElevation
+              className="scale-90 rounded-3xl border border-solid border-[#4639bf] bg-[#9757FF] px-6 pt-3 pb-4 font-bold transition-all duration-300 hover:scale-95 hover:bg-[#4639bf]"
+              variant="contained"
+              onClick={onAddressClicked}
+            >
+              <Typography color="white">Connect Wallet to Continue</Typography>
+            </Button>
+          </div>
+        </Paper>
+
+
+
+      )}
+      {unlockOpen && <Unlock modalOpen={unlockOpen} closeModal={closeUnlock} />}
     </div>
   );
 }
