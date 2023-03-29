83f3dfef2691af84a76a67f7ece70ed1f6dc1df4
MohamadAli-dev <azzam@localhost.localdomain>
2023-03-29 21:25:13 +0300

    add bera logo / change bg

--- Frontend-v1-Original/pages/configure/index.tsx
+++ Frontend-v1-Original/pages/configure/index.tsx
@@ -5,9 +5,9 @@ import './configure.module.css'
 function Configure() {
   return (
     <div className="back w-100 h-100 relative flex min-h-screen flex-col items-center justify-center bg-none">
-      <Image src="/images/logo-icon-configure.png" width="100%" height="100%" />
-      <div className="pointer-events-none absolute bottom-4 left-4 h-[512px] w-[512px] opacity-50 blur-[256px]" />
-      <div className="pointer-events-none absolute top-4 right-4 h-[512px] w-[512px] opacity-50 blur-[256px]" />
+      <Image src="/images/logo_icon.svg" width="100%" height="100%" />
+      {/* <div className="pointer-events-none absolute bottom-4 left-4 h-[512px] w-[512px] opacity-50 blur-[256px]" />
+      <div className="pointer-events-none absolute top-4 right-4 h-[512px] w-[512px] opacity-50 blur-[256px]" /> */}
     </div>
   );
 }
